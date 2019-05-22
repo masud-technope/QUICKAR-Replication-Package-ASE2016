2cdc290a6ce21429542f45148a3ef91bf0432ff4
unknown <masudcseku@gmail.com>
2019-05-21 19:56:46 -0600

    Base version of quickar

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# QUICKAR-Replication-Package-ASE2016
-Automatic Query Reformulation for Concept Location using Crowdsourced Knowledge
+## QUICKAR: Automatic Query Reformulation for Concept Location using Crowdsourced Knowledge
+
+Accepted Paper at ASE 2016
+----------------------------
+```
+QUICKAR: Automatic Query Reformulation for Concept Location Using Crowdsourced Knowledge
+Mohammad Masudur Rahman and Chanchal K. Roy
+```
+**Abstract:** During maintenance, software developers deal with numerous change requests made by the users of a software system. Studies show that the developers find it challenging to select appropriate search terms from a change request during concept location. In this paper, we propose a novel technique--QUICKAR--that automatically suggests helpful reformulations for a given query by leveraging the crowdsourced knowledge from Stack Overflow. It determines semantic similarity or relevance between any two terms by analyzing their adjacent word lists from the programming questions of Stack Overflow, and then suggests semantically relevant queries for concept location. Experiments using 510 queries from two software systems suggest that our technique can improve or preserve the quality of 76% of the initial queries on average which is promising. Comparison with one baseline technique validates our preliminary findings, and also demonstrates the potential of our technique.
+
+Experimental Data
+--------------------
+**Subject Systems (2)**
+
+ - ecf (222)
+ - eclipse.pde.ui (288)
+ 
+ **Baseline Method**
+ 
+ - ```Query```: Baseline queries used in the experiment
+ - ```QE```: Query Effectiveness achieved by baseline queries
+ 
+ **QUICKAR**
+ 
+ - ```Query```: Queries suggested by QUICKAR
+ - ```QE```: Query Effectiveness achieved by QUICKAR queries
+ 
+ Please cite our work as
+ ----------------
+ ```
+@inproceedings{ase2016masud, 
+author = {Rahman, M. M. and Roy, C. K. }, 
+title = {{QUICKAR: Automatic Query Reformulation for Concept Location Using Crowdsourced Knowledge}}, 
+booktitle = {Proc. ASE}, 
+year = {2016}, 
+pages = {220--225} }
+ ```
+
+## Something not working as expected?
+
+Contact:  **Masud Rahman**  ([masud.rahman@usask.ca](mailto:masud.rahman@usask.ca))
+
+OR
+
+Create an issue from  [here](https://github.com/masud-technope/QUICKAR-Replication-Package-ASE2016/issues/new)
---
+++ index.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<link href="../css/style1.css" type="text/css" rel="stylesheet" />
+
+<title>QUICKAR: Automatic Query Reformulation for Concept Location using Crowdsourced Knowledge </title>
+<style type="text/css">
+hl
+{
+ color:#0066FF;
+ font-size:18px;
+}
+.important
+{
+ color:#FF0000;
+ font-weight:bold;
+}
+body
+{
+margin-top:100px;
+}
+
+
+</style>
+</head>
+<body>
+<table border="0" cellpadding="0" cellspacing="0" width="1020" align="center">
+<tr>
+<td width="100">
+<img src="img/quickar.png" height="100" width="89" border="0" />
+</td> 
+<td><h1>QUICKAR: Automatic Query Reformulation for Concept Location using Crowdsourced Knowledge </h1></td>
+</tr>
+
+<tr>
+<td colspan="2">
+<p align="justify" class="rounded2" >
+<strong>Abstract:</strong> During maintenance, software developers deal with numerous change requests made by the users of a software system.
+Studies show that the developers find it challenging to select appropriate search terms from a change request during concept location.  
+In this paper, we propose a novel technique--QUICKAR--that automatically suggests helpful reformulations for a given query by leveraging the crowdsourced knowledge from Stack Overflow.
+It determines semantic similarity or relevance between any two terms by analyzing their adjacent word lists from the programming questions of Stack Overflow, and 
+then suggests semantically relevant queries for concept location. Experiments using 510 queries from two software systems suggest that our technique
+can improve or preserve the quality of 76% of the initial queries on average which is promising. Comparison with one baseline technique validates our preliminary findings, and also demonstrates the potential of our technique.
+</p>
+</td>
+</tr>
+<tr>
+<td>
+&nbsp;
+<spacer></spacer>
+</td>
+</tr>
+<tr>
+<td colspan="2">
+<h3>Experimental Data</h3>
+<hr />
+<ul>
+<li><a href="expdata/ecf.txt">ECF</a></li>
+<li><a href="expdata/eclipse.pde.ui.txt">ECLIPSE.PDE.UI</a></li>
+</ul>
+</td>
+</tr>
+<tr>
+<td colspan="2">
+<h3>Prototype</h3>
+<hr />
+Work in progress.
+<br />
+</td>
+</tr>
+
+
+<tr>
+<td colspan="2">
+<spacer></spacer>
+<h3 style="font-weight:bold">Related Publication(s)</h3>
+<hr />
+<ul>
+<li>
+<strong>M. Masudur Rahman</strong> and C.K. Roy, "QUICKAR: Automatic Query Reformulation for Concept Location Using Crowdsourced Knowledge", In Proceeding of The 31st IEEE/ACM International Conference on Automated Software Engineering (<strong>ASE 2016</strong>), pp. 220--225, Singapore, September 2016
+</li>
+</ul>
+<div class="rounded2">
+<pre>
+@inproceedings{ase2016masud, 
+author = {Rahman, M. M. and Roy, C. K. }, 
+title = {{QUICKAR: Automatic Query Reformulation for Concept Location Using Crowdsourced Knowledge}}, 
+booktitle = {Proc. ASE}, 
+year = {2016}, 
+pages = {220--225} } 
+</pre>
+</div>
+</td>
+</tr>
+
+
+
+<tr>
+<td colspan="2">&nbsp;</td>
+</tr>
+
+</tr>
+<tr>
+<td colspan="3">
+<h4 style="text-align:center">
+<a style="text-decoration:none; cursor:pointer" href="../tools">&larr; Check out other tools by Masud Rahman</a>
+</h4>
+</td>
+</tr>
+
+<tr>
+<td colspan="2">
+<p>
+&copy; Masud Rahman, Computer Science, University of Saskatchewan, Canada.
+</p>
+</td>
+</tr>
+
+</table>
+</body>
+</html>
